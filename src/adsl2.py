449a2ef3bb07f1042f55c118333d46a7ddbaea12
wgy-ts <wgy-ts@163.com>
2016-03-29 17:52:41 +0800

    coding

--- src/adsl2.py
+++ src/adsl2.py
@@ -1,18 +1,16 @@
 # -*- coding:utf-8 -*-
 
-import os, time
+import os
 
 
 class Adsl(object):
     def connect(self):
         cmdstr = "/sbin/pppoe-start"
         os.system(cmdstr)
-        time.sleep(10)
 
     def disconnect(self):
         cmdstr = "/sbin/pppoe-stop"
         os.system(cmdstr)
-        time.sleep(5)
 
     def reconnect(self):
         self.disconnect()
